d7ef4a3a1bd9cac2a3df2f42f9ae4d1f546962d9
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-11 17:05:26 +0000

    Add test for gc_content

--- tests/testthat/test-fastqR.R
+++ tests/testthat/test-fastqR.R
@@ -16,3 +16,9 @@ test_that("Reading FastQ files", {
   expect_error(read_fastq(system.file("mismatched_lengths.fq", package = "fastqR")))
   expect_error(read_fastq(system.file("wrong_extension.fa", package = "fastqR")))
 })
+
+
+test_that("Check GC content", {
+  expect_equal(gc_content("GGATCTTAGG"), 50)
+  expect_warning(gc_content("GGATCNTTAGG"))
+})

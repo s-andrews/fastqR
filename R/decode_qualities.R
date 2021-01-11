edfc31431ab5e6f4d589da94dfc25aa4d7d889fc
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-11 13:56:07 +0000

    Fix code in decode_qualities

--- R/decode_qualities.R
+++ R/decode_qualities.R
@@ -1,7 +1,7 @@
 decode_qualities <- function(qualities, offset=33) {
 
-  assert_that(is.scalar(offset))
-  assert_that(is.number(offset))
+  assertthat::assert_that(assertthat::is.scalar(offset))
+  assertthat::assert_that(assertthat::is.number(offset))
 
   if (!(offset==33 | offset==64)) {
     stop("Offset can only be 33 or 64")

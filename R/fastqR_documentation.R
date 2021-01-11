8d9f2489a16fadee778e39441df0e07ec81fe12e
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-11 15:43:54 +0000

    Added package level documentation

---
+++ R/fastqR_documentation.R
@@ -0,0 +1,17 @@
+#' fastqR: A package to read and manipulate FastQ files
+#'
+#' This package works with FastQ format files.  It can read these
+#' into a tidyverse compatible data structure and provides
+#' functions for manipulating the data within them.
+#'
+#' @section fastqR functions
+#'
+#' read_fastq
+#'
+#' gc_content
+#'
+#' decode_qualities
+#'
+#' @docType package
+#' @name fastqR
+NULL
